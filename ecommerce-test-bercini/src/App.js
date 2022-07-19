0e64b5e30b2d2e8165c19568d8ed7f209e1c6e9c
Nazareno Bercini <89424150+Nazareno-Bercini@users.noreply.github.com>
2022-07-19 20:25:04 -0300

    Desafío: Menú e-commerce

--- ecommerce-test-bercini/src/App.js
+++ ecommerce-test-bercini/src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
-
+import NavBar from './components/NavBar/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h1>Bienvenido a Camisetas Bercini</h1>
 
     </div>
---
+++ ecommerce-test-bercini/src/components/NavBar/NavBar.js
@@ -0,0 +1,32 @@
+// Upper CamelCase
+
+const NavBar = () => {
+    return(
+
+        <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">B.Camisetas</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Primera Division</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contacto</a>
+        </li>
+      </ul>
+
+    </div>
+  </div>
+</nav>
+
+    )
+}
+
+export default NavBar
